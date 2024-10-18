421442817806d54bededb2cf2ce40f600b202a3b
Pedro Bittencourt <165230428+Pedrookkss@users.noreply.github.com>
2024-10-17 22:37:03 -0300

    Edit1

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,9 +15,6 @@ function loadPokemonItens (offset, limit) {
                 <span class="name">${pokemon.name}</span>
                     
                 <div class="detail">
-                    <ol class="types">
-                        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
-                    </ol>
                         <img src=${pokemon.photo} alt="${pokemon.name}">
                 </div>
     
